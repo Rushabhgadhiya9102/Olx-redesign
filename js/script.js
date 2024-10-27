ebab51edeec9f135ae989b21a65dbd7ce83ec853
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-10-28 00:01:10 +0530

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(".more-less,.show-less").hide();
 $(".show-more-less").on("click", function(){
+     $(".show-more, .show-less").toggle();
     $(".more-less").slideToggle(1000);
-    $(".show-more, .show-less").toggle();
 });
