c31fa9bb6425759b4b3ade96573e7956ea34c439
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-11-10 14:49:31 +0530

    done

--- js/script.js
+++ js/script.js
@@ -38,7 +38,6 @@ $(document).ready(function () {
 // CART SECTION
 
 $(document).ready(function(){
-
    const cart = $(".cart-section");
    const home = $("#home-page");
    cart.hide(); 
@@ -54,23 +53,98 @@ $(document).ready(function () {
     var cardClone = $($(this).attr('for')).clone();
     
     cardClone.css("width","310px");
-    // cardClone.find(".add-to-cart").remove();
-
     // Append the cloned card to the cart container
     $("#cart-container").append(cardClone);
     $(".offer").append();
-    alert('item saved');
+    showPopover('item saved')
   });
 });
 
 // TOAST
-
-const toastTrigger = document.getElementById("liveToastBtn");
+const toastTrigger1 = document.getElementById("liveToastBtn1");
+const toastTrigger2 = document.getElementById("liveToastBtn2");
+const toastTrigger3 = document.getElementById("liveToastBtn3");
+const toastTrigger4 = document.getElementById("liveToastBtn4");
+const toastTrigger5 = document.getElementById("liveToastBtn5");
+const toastTrigger6 = document.getElementById("liveToastBtn6");
+const toastTrigger7 = document.getElementById("liveToastBtn7");
+const toastTrigger8 = document.getElementById("liveToastBtn8");
+const toastTrigger9 = document.getElementById("liveToastBtn9");
+const toastTrigger10 = document.getElementById("liveToastBtn10");
+const toastTrigger11 = document.getElementById("liveToastBtn11");
+const toastTrigger12 = document.getElementById("liveToastBtn12");
 const toastLiveExample = document.getElementById("liveToast");
 
-if (toastTrigger) {
+if (toastTrigger1) {
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
-  toastTrigger.addEventListener("click", () => {
+  toastTrigger1.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger2) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger2.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger3) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger3.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger4) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger4.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger5) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger5.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger6) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger6.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger7) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger7.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger8) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger8.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger9) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger9.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger10) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger10.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+
+if (toastTrigger11) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger11.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+if (toastTrigger12) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger12.addEventListener("click", () => {
     toastBootstrap.show();
   });
 }
